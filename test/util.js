16f8cf1bc4b818cfe3a59617739875d98b5c20df
Luke Segars <luke@theironyard.com>
2015-11-28 13:10:42 -0500

    npm test now runs code coverage tests as well
    Also reorganized repo structure a bit.

--- test/util.js
+++ test/util.js
@@ -1,14 +1,30 @@
-var assert = require("assert");
-var util = require("../util");
+/* jslint node: true, mocha: true */
+//var assert = require("assert");
+require("blanket");
+var util = require("../js/util");
+require("../js/routes");
+require("../js/bouncer");
 var redis = require("redis");
+var expect = require("chai").expect;
 
 describe("util.js", function () {
     var client;
 
     // Initialize a Redis client connection to localhost before running any
     // tests.
-    before('initialize redis client', function () {
-        client = redis.createClient();
+    before('initialize redis client', function (done) {
+        try {
+            client = redis.createClient();
+            expect(client).to.not.be.undefined;
+
+            client.keys("test:*", function (error, results) {
+                client.del(results);
+                done();
+            });
+        } catch (e) {
+            expect(e).to.be.undefined;
+            done();
+        }
     });
 
     // Once all tests are completed, remove everything in the testing keyspace
@@ -24,63 +40,65 @@ describe("util.js", function () {
         it("created object has all expected fields set", function (done) {
             var shortlink = "test:goo";
             var url = "https://google.com";
+            this.timeout(5000);
 
-            var entry = util.create({
-                shortlink: shortlink,
-                url: url,
-            }, function (newest) {
-                assert.equal(true,
-                    newest.shortlink == shortlink &&
-                    newest.url == url &&
-                    newest.createdOn > 0 &&
-                    newest.domain == "google.com" &&
-                    newest.title !== "Unknown");
+            try {
+                util.create({
+                    shortlink: shortlink,
+                    url: url,
+                }, function (newest) {
+                    expect(newest.shortlink).to.be.equal(shortlink);
+                    expect(newest.url).to.be.equal(url);
+                    expect(newest.createdOn).to.be.above(0);
+                    expect(newest.domain).to.be.equal("google.com"); // Set dynamically
+                    expect(newest.title).to.be.equal("Google"); // Set dynamically
 
+                    done();
+                });
+            } catch (e) {
+                expect(e).to.be.undefined;
                 done();
-            });
+            }
         });
 
         it("trying to create a shortlink with no url fails", function (done) {
-            var shortlink = "test:car";
+            var shortlink = "test:shortlink_no_url";
 
             try {
-                var entry = util.create({
+                util.create({
                     shortlink: shortlink,
                 }, function (newest) {
-                    assert.equal(true, false);
+                    expect(newest).to.be.undefined;
                     done();
                 });
             } catch (e) {
                 if (e.name == "AssertionError") throw e;
-                else assert.equal(true, true);
                 done();
             }
         });
 
         it("trying to create a shortlink with no shortlink fails", function (done) {
             try {
-                var entry = util.create({
+                util.create({
                     url: "https://google.com",
                 }, function (newest) {
-                    assert.equal(true, false);
+                    expect(newest).to.be.undefined;
                     done();
                 });
             } catch (e) {
                 if (e.name == "AssertionError") throw e;
-                else assert.equal(true, true);
                 done();
             }
         });
 
         it("trying to create a shortlink with no info fails", function (done) {
             try {
-                var entry = util.create({}, function (newest) {
-                    assert.equal(true, false);
-                    done()
+                util.create({}, function (newest) {
+                    expect(newest).to.be.undefined;
+                    done();
                 });
             } catch (e) {
                 if (e.name == "AssertionError") throw e;
-                else assert.equal(true, true);
                 done();
             }
         });
