a51a00152dfb69fd0904636d65ed77c50d8fa60d
Luke Segars <luke@theironyard.com>
2015-11-28 21:02:14 -0500

    Added some tests and fixed issues that come up. Added namespace to all keys.
    Standard keys all have 'bouncer:' prefix added to them. Testing entries have
    'test:' appended as a prefix.

--- js/bouncer.js
+++ js/bouncer.js
@@ -1,75 +1,7 @@
 /* jslint node: true */
-"use strict";
-var url = require("url");
-var http = require("http");
-var routes = require("./routes");
+var bouncer = require('./server');
 
-var PORT = 8080;
-
-/**
- * Filtering function that determines whether a particular request should get a response.
- */
-function dropRequest(request) {
-    var path = url.parse(request.url);
-    return path.pathname === "/favicon.ico";
-}
-
-/**
- * Router function determines which handler needs to process the request. All handlers are
- * stored in routes.js.
- */
-function router(request, response) {
-    var path = url.parse(request.url);
-
-    // If the request is not one we want to respond to, ignore it.
-    if (dropRequest(request)) {
-        response.end();
-        return;
-    }
-
-    // When no shortlink is provided.
-    //   Ex: Expected path: http://localhost/
-    if (path.pathname === "/") {
-        routes.main(request, response, function (resp) {
-            resp.end();
-        });
-        // Static content handler.
-    } else if (path.pathname.indexOf("/static") === 0) {
-        routes.fetchFile(request, response, function (resp) {
-            resp.end();
-        });
-        // Edit an existing shortlink. 
-        //   Ex: Expected path is /edit/{shortlink}
-    } else if (path.pathname.indexOf("/edit") === 0) {
-        routes.edit(request, response, function (resp) {
-            resp.end();
-        });
-        // Create a new shortlink (depends on POST'd parameters, usually from a form).
-        //   Ex: Only matches with /create.
-    } else if (path.pathname === "/create") {
-        routes.create(request, response, function (resp) {
-            resp.end();
-        });
-    } else if (path.pathname.indexOf("/debug") === 0) {
-        routes.debug(request, response, function (resp) {
-            resp.end();
-        });
-        // Any time a shortlink is provided
-        //   Ex: http://localhost/myfavoritelink
-    } else {
-        routes.redirect(request, response, function (resp) {
-            resp.end();
-        });
-    }
-}
-
-/**
- * Initialize a server that uses the router() function to
- * process and route requests.
- */
-
-var server = http.createServer(router);
-
-server.listen(PORT, function () {
-    console.log("Server is listening on port " + PORT);
+var server = bouncer.createServer();
+bouncer.init(server, function () {
+    console.log("Bouncer server is running.");
 });
--- js/util.js
+++ js/util.js
@@ -8,6 +8,8 @@ var URL = require("url");
 var request = require("request");
 var cheerio = require("cheerio");
 
+var NAMESPACE = "bouncer";
+
 /**
  * Core utility functionality that's invoked by request handlers in the main app. Note
  * that there may be multiple paths to call these functions, and ideally all storage
@@ -23,13 +25,19 @@ var Entry = {
         if (entry.url === null) throw Error("You must provide a URL for the shortlink.");
 
         var newest = {
-            shortlink: entry.shortlink,
+            shortlink: (entry.shortlink.split(':').length > 1) ? entry.shortlink.split(':')[1] : entry.shortlink,
             url: entry.url,
             createdOn: Date.now(),
             domain: function (url) {
                 return URL.parse(url).hostname;
             }(entry.url),
             title: null,
+            namespace: (entry.shortlink.split(':').length > 1) ? entry.shortlink.split(':')[0] : NAMESPACE,
+
+            // Generate the key that should be used to store this entry.
+            key: function () {
+                return addNamespace(this.namespace, this.shortlink);
+            },
         };
 
         request(entry.url, function (error, response, body) {
@@ -37,10 +45,8 @@ var Entry = {
                 var doc = cheerio.load(body, {
                     decodeEntities: true,
                 });
-                console.log(doc('title').text());
                 newest.title = doc('title').text().replace(/[^\x00-\x7F]/g, "");
             } else {
-                console.log("[EntryLookup] Specified URL doesn't exist: " + entry.url);
                 newest.title = "Unknown";
             }
 
@@ -48,6 +54,10 @@ var Entry = {
         });
     },
 };
+
+function addNamespace(ns, token) {
+    return ns + ":" + token;
+}
 
 /**
  * Create a new shortlink.
@@ -62,7 +72,11 @@ exports.create = function (entry, callback) {
         // Store the new object.
         // TODO: only create a single client (I think this is currently one connection per request).
         var client = redis.createClient();
-        client.set(newest.shortlink, entryPb.serialize(newest, "bouncer.Entry"));
+
+        client.set(
+            newest.key(),
+            entryPb.serialize(newest, "bouncer.Entry")
+        );
 
         callback(newest);
     });
@@ -73,15 +87,20 @@ exports.create = function (entry, callback) {
  * return null.
  */
 exports.get = function (shortlink, callback) {
+    exports.getFromNamespace(NAMESPACE, shortlink, callback);
+};
+
+exports.getFromNamespace = function (ns, shortlink, callback) {
     var client = redis.createClient(null, null, {
         return_buffers: true,
     });
 
     client.on("error", function (err) {
         console.log("Error " + err);
+        throw err;
     });
 
-    client.get(shortlink, function (err, reply) {
+    client.get(addNamespace(ns, shortlink), function (err, reply) {
         // If there's an issue, return the error.
         if (err !== null) {
             callback(err);
@@ -99,20 +118,35 @@ exports.get = function (shortlink, callback) {
 };
 
 exports.getAll = function (callback) {
+    exports.getAllFromNamespace(NAMESPACE, callback);
+};
+
+exports.getAllFromNamespace = function (ns, callback) {
     var client = redis.createClient(null, null, {
         return_buffers: true,
     });
 
     // Get entries for all shortlinks.
-    client.keys("*", function (err, reply) {
+    client.keys(addNamespace(ns, "*"), function (err, reply) {
         var keys = reply.map(function (item) {
-            return item.toString();
+            item = item.toString();
+
+            // A namespace should always be specified. Split the key out into the namespace
+            // and token.
+            if (item.split(':').length > 1) {
+                return {
+                    namespace: item.split(':')[0],
+                    token: item.split(':')[1],
+                };
+            } else {
+                throw Error('No namespace specified.');
+            }
         });
 
         // Generate a list of functions that retrieve entries, one per shortlink.
         async.parallel(keys.map(function (item) {
                 return function (db_callback) {
-                    exports.get(item, function (err, val) {
+                    exports.getFromNamespace(item.namespace, item.token, function (err, val) {
                         db_callback(err, val);
                     });
                 };
@@ -122,4 +156,4 @@ exports.getAll = function (callback) {
                 callback(items);
             });
     });
-}
+};
